3e01c292b823382ce859ec232819e648a0bdffde
robcranfill <robcranfill@gmail.com>
2025-03-28 22:27:30 -0700

    start of fsm

--- cpsr16.py
+++ cpsr16.py
@@ -12,23 +12,41 @@ import sys
 import time
 
 # adafruit libs
-import board
-import busio
-
 import audiobusio
 import audiocore
 import audiomixer
+import board
+import busio
+
+import keypad
+from digitalio import DigitalInOut, Pull
+import adafruit_debouncer
 
 
-DATA_FILE_NAME = "rhythms-v2-b.dict"
+# TODO: make this a variable?
+# kind of a misnomer; should be ticks_per_measure, or something.
+BEATS_PER_MEASURE = 16
+
+
+DATA_FILE_NAME = "rhythms.dict"
 
 NOT_PLAYING_DELAY = 0.1
 
 
 # for I2S audio with external I2S DAC board
-AUDIO_OUT_I2S_BIT  = board.D9
-AUDIO_OUT_I2S_DATA = board.D11
-AUDIO_OUT_I2S_WORD = board.D10
+
+# for my RP2350 testbed:
+# AUDIO_OUT_I2S_BIT  = board.D9
+# AUDIO_OUT_I2S_DATA = board.D11
+# AUDIO_OUT_I2S_WORD = board.D10
+
+# for Pico
+AUDIO_OUT_I2S_BIT  = board.GP8
+AUDIO_OUT_I2S_DATA = board.GP10
+AUDIO_OUT_I2S_WORD = board.GP9
+
+SWITCH_1 = board.GP28
+SWITCH_2 = board.GP27
 
 BEAT_NAMES = ["1", "e", "and", "uh", "2", "e", "and", "uh", "3", "e", "and", "uh", "4", "e", "and", "uh"]
 
@@ -65,6 +83,40 @@ def init_audio(n_voices):
 
     # We must also return the "audio" object so it doesn't get garbage collected!
     return au, mx
+
+def init_footswitch_debouncer():
+
+    PINS = (
+        SWITCH_1,
+        SWITCH_2
+        )
+
+    keys = []
+    for pin in PINS:
+        dio = DigitalInOut(pin)
+        dio.pull = Pull.UP
+        keys.append(adafruit_debouncer.Button(dio))
+
+    k = 0
+    print("\nlooking at footswitch....")
+    while k < 10:
+    # while True:
+        # print(k)
+        for i in range(len(PINS)):
+            keys[i].update()
+            if keys[i].fell:
+                print(f"fell! {PINS[i]}")
+                k += 1
+            elif keys[i].rose:
+                print(f"rose! {PINS[i]}")
+                k += 1
+    return keys
+
+
+def init_footswitch():
+    """Using 'keypad'"""
+    keys = keypad.Keys((SWITCH_1,SWITCH_2), value_when_pressed=False, pull=True)
+    return keys
 
 
 def load_setup(setups, setup_name):
@@ -115,16 +167,16 @@ def get_setup_names(setups):
 def make_beats(pad_name, beat_pattern, channel):
     """"
     Given the pad name and beat pattern, add all non-zero hits to a list of hits.
-    Return a 16-slot list of beats like (channel, vol) for this pad.
+    Return a BEATS_PER_MEASURE-slot list of beats like (channel, vol) for this pad.
     """
     # print(f"   make_beats for pad '{pad_name}': '{beat_pattern}'")
 
-    beat_list = [()] * 16
+    beat_list = [()] * BEATS_PER_MEASURE
     j = -1 # The input is broken into 4-char chunks for readability; j is index into beat_pattern string.
 
     i_track = channel # OK?
 
-    for beat in range(16):
+    for beat in range(BEATS_PER_MEASURE):
         if beat % 4 == 0:
             j += 1
         # print(f"Looking at {beat=} from char {j}...")
@@ -149,7 +201,7 @@ def load_beats_for_patterns(setup, wav_table):
        ...
        }
     where beats are like:
-        beats = ((),) * 16
+        beats = ((),) * BEATS_PER_MEASURE
         beats[0] = ((0, wav_dict["snare"], 5),  (0, wav_dict["kick"], 9))
         beats[4] = ((0, wav_dict["snare"], 9))
         beats[6] = ((0, wav_dict["snare"], 9))
@@ -183,11 +235,11 @@ def load_beats_for_patterns(setup, wav_table):
 
         # Note: LESSON LEARNED
         # The following is right; what was wrong was
-        #   track_hits = [[]] * 16
+        #   track_hits = [[]] * BEATS_PER_MEASURE
         #  which is a list of *the same object*
         #  and does very weird stuff! :-/
         #
-        track_hits = [[] for _ in range(16)]
+        track_hits = [[] for _ in range(BEATS_PER_MEASURE)]
 
         for t in range(len(tracks)):
             for b in range(len(tracks[t])):
@@ -204,10 +256,27 @@ def load_beats_for_patterns(setup, wav_table):
     return all_beats
 
 
+def handle_events(drum_machine, switch_list):
+
+    event = switch_list.events.get()
+    # event will be None if nothing has happened.
+    if event:
+        print(f" ***** {event}")
+        if event.pressed and event.key_number == 0:
+            drum_machine.set_playing(not drum_machine.is_playing())
+            print(f" set dm to {drum_machine.is_playing()=}")
+
+
 
 ###########################################################
 
 def main():
+
+    import DM_state
+    dm = DM_state.DM_state()
+
+    # switches = init_footswitch_debouncer()
+    switches = init_footswitch()
 
     # TODO: needed? "Wait a little bit so USB can stabilize and not glitch audio"
     time.sleep(2)
@@ -251,6 +320,7 @@ def main():
     print(f"\nPattern: {pattern}\n")
 
     # 120 BPM, sorta
+    # TODO: obviusly needs to be variable - how?
     SLEEP_TIME = 1/8
 
     # this is just for printing the nice beat name like "one" or "and"
@@ -261,14 +331,35 @@ def main():
     playing = True
     while True:
 
-        # if not playing:
-        #     print("Not playing!")
-        #     time.sleep(NOT_PLAYING_DELAY)
-        #     playing = check_gesture(gesture_sensor)
-        #     continue
+        if not dm.is_playing():
+            # print("not playing")
+            time.sleep(NOT_PLAYING_DELAY)
+
+            handle_events(dm, switches)
+
+            continue
+
+        # for debouncer approach - no good?
+        #
+        # for switch in switches:
+        #     switch.update()
+        #     if switch.rose:
+        #         print(f"\n\n\n{switch} rose!\n\n\n")
+        #     elif switch.fell:
+        #         print(f"{switch} fell!")
+        #     elif switch.long_press:
+        #         print("Long Press")
+        #     elif switch.short_count != 0:
+        #         print("Short Press Count =", switch.short_count)
+        #     elif switch.long_press and switch.short_count == 1:
+        #         print("That's a long double press !")
 
         # for hit_list in pattern:
-        for beat in range(16):
+        for beat in range(BEATS_PER_MEASURE):
+
+            handle_events(dm, switches)
+            if not dm.is_playing: # uh, what?
+                break
 
             hit_list = pattern[beat]
             print(f"  Hit list: {hit_list}")
@@ -282,7 +373,7 @@ def main():
 
             if len(hit_list) > 0:
                 print(f" BEAT '{BEAT_NAMES[beat]}': {hit_list=}")
-                beat = (beat+1) % 16
+                beat = (beat+1) % BEATS_PER_MEASURE
 
                 # for channel, volume in hit_list:
                 for cv_tuple in hit_list:
@@ -292,7 +383,7 @@ def main():
 
                         wav = wav_table[channel]
 
-                        print(f"  {channel=} @{volume=}")
+                        # print(f"  {channel=} @{volume=}")
 
                         if volume != 0:
                             # print(f"     playing {track_index=} @ {volume=} ")
